a38c19c67269d09804ea5e9466a3c3eb477e3e8a
Pedro Starling Freitas <pedrostarling2000@gmail.com>
2023-10-23 12:43:28 -0300

    fix Joint formatting

--- src/Joint.zig
+++ src/Joint.zig
@@ -5,6 +5,13 @@ const Body = @import("Body.zig");
 pub export fn Joint(comptime T: type) type {
     return struct {
         const Self = @This();
+        M: Mat22(T),
+        localAnchor1: Vec2(T),
+        localAnchor2: Vec2(T),
+        r1: Vec2(T),
+        r2: Vec2(T),
+        bias: Vec2(T),
+
         body1: Body(T),
         body2: Body(T),
     };
