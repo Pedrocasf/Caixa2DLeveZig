ab1cd8fbda4753d0b99b17215e02bdb1cbc9b055
Pedro Starling Freitas <pedrostarling2000@gmail.com>
2023-10-24 00:55:28 -0300

    change main to lib

---
+++ src/lib.zig
@@ -0,0 +1,10 @@
+const std = @import("std");
+const MathUtils = @import("MathUtils.zig");
+const NumT = f32;
+pub const Vec2 = MathUtils.Vec2(NumT);
+pub const Mat22 = MathUtils.Mat22(NumT);
+pub const Arbiter = @import("Arbiter.zig").Arbiter(NumT);
+pub const ArbiterKey = @import("Arbiter.zig").ArbiterKey(NumT);
+pub const Body = @import("Body.zig").Body(NumT);
+pub const Joint = @import("Joint.zig").Joint(NumT);
+pub const World = @import("World.zig").World(NumT);
