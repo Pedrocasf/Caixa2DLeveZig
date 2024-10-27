d3254d8f9d75ae2817f1a6ba4512ccea75f071c8
Pedro Starling Freitas <pedrostarling2000@gmail.com>
2024-10-27 16:23:32 -0300

    change some functions to maxFloat

--- src/Body.zig
+++ src/Body.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
-const maxInt = std.math.maxInt;
 const Math = @import("MathUtils.zig");
+const maxFloat = std.math.floatMax;
 const Vec2 = Math.Vec2;
 pub fn Body(comptime T: type) type {
     return struct {
@@ -18,7 +18,20 @@ pub fn Body(comptime T: type) type {
         I: T,
         invI: T,
         pub fn init() Self {
-            Self{ .position = Vec2(T).init(0, 0), .rotation = 0, .velocity = Vec2(T).init(0, 0), .angularVelocity = 0, .force = Vec2(T).init(0, 0), .torque = 0, .friction = 1, .width = Vec2(T).init(1, 1), .mass = maxInt(T), .invMass = 0, .I = maxInt(T), .invI = 0 };
+            Self{
+                .position = Vec2(T).init(0, 0),
+                .rotation = 0,
+                .velocity = Vec2(T).init(0, 0),
+                .angularVelocity = 0,
+                .force = Vec2(T).init(0, 0),
+                .torque = 0,
+                .friction = 1,
+                .width = Vec2(T).init(1, 1),
+                .mass = maxFloat(T),
+                .invMass = 0,
+                .I = maxFloat(T),
+                .invI = 0,
+            };
         }
         pub fn addForce(self: Self, f: Vec2(T)) void {
             self.force.acc(f);
@@ -33,13 +46,13 @@ pub fn Body(comptime T: type) type {
             self.friction = 1;
             self.width = w;
             self.mass = m;
-            if (self.mass < maxInt(T)) {
+            if (self.mass < maxFloat(T)) {
                 self.invMass = 1 / self.mass;
                 self.I = self.mass * (self.width.x * self.width.x + self.width.y * self.width.y) / 12;
                 self.invI = 1 / self.I;
             } else {
                 self.invMass = 0;
-                self.I = maxInt(T);
+                self.I = maxFloat(T);
                 self.invI = 0;
             }
         }
